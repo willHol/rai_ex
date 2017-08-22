1f047ef08694c4abd4657db9ec4ebe0867c2e7c6
William Holmes <thenecroyeti@gmail.com>
2017-08-22 22:38:35 +1200

    error clause

--- lib/rai_ex/tools.ex
+++ lib/rai_ex/tools.ex
@@ -14,18 +14,18 @@ defmodule RaiEx.Tools do
 	def change_password(wallet, current_pwd, password) do
 		with {:ok, %{"valid" => "1"}} <- RaiEx.password_enter(wallet, current_pwd),
 			 	 {:ok, %{"changed" => "1"}} <- RaiEx.password_change(wallet, password)
-			 	 do {:ok, wallet} else {:ok, reason} -> {:error, reason} end
+			 	 do {:ok, wallet} else {_, reason} -> {:error, reason} end
 	end
 
 	def wallet_create_encrypted(password) do			   
 		with {:ok, %{"wallet" => wallet}} <- RaiEx.wallet_create,
 				 {:ok, ^wallet} <- (:timer.sleep(@delay); change_password(wallet, "", password))
-			   do {:ok, wallet} else {:ok, reason} -> {:error, reason} end
+			   do {:ok, wallet} else {_, reason} -> {:error, reason} end
 	end
 
 	def wallet_add_adhoc(wallet) do
 		with {:ok, %{"private" => priv, "public" => pub, "account" => acc}} <- RaiEx.key_create,
 			   {:ok, %{"account" => ^acc}} <- RaiEx.wallet_add(wallet, priv)
-			   do {:ok, %{"private" => priv, "public" => pub, "account" => acc}} else {:ok, reason} -> {:error, reason} end
+			   do {:ok, %{"private" => priv, "public" => pub, "account" => acc}} else {_, reason} -> {:error, reason} end
 	end
 end
