7f705edbf6f95bc407e5b87a65da44147e820c1d
William <thenecroyeti@gmail.com>
2017-10-12 15:49:08 +1300

    duplex mapping

--- lib/rai_ex/Base.ex
+++ lib/rai_ex/Base.ex
@@ -2,10 +2,12 @@ defmodule RaiEx.Base do
   @mappings (
     {_i, mappings} =
       "13456789abcdefghijkmnopqrstuwxyz"
-      |> String.split("")
-      |> Enum.drop(-1)
+      |> String.split("", trim: true)
       |> Enum.reduce({0, %{}}, fn letter, {i, map} -> 
-           {i + 1, Map.put(map, letter, <<i :: size(5)>>)}
+           {
+            i + 1,
+            Map.merge(map, %{letter => <<i :: size(5)>>, <<i :: size(5)>> => letter})
+          }
          end)
 
     mappings
@@ -13,8 +15,7 @@ defmodule RaiEx.Base do
 
   def decode!(string) do
     string
-    |> String.split("")
-    |> Enum.drop(-1)
+    |> String.split("", trim: true)
     |> Enum.reduce(<<>>, &(<<&2::bitstring, @mappings[&1]::bitstring>>))
   end
 
@@ -25,6 +26,10 @@ defmodule RaiEx.Base do
     rescue
       e in ArgumentError -> {:error, :badarg}
     end
+  end
+
+  def encode(binary) do
+    
   end
 
   def character_mappings, do: @mappings
