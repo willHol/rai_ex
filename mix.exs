5fcf1be1fcec4f2644e2d754cb10f935e8d15a1f
william <thenecroyeti@gmail.com>
2017-10-14 23:41:44 +1300

    versioning

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule RaiEx.Mixfile do
   def project do
     [
       app: :rai_ex,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps(),
