da8671e6eabfb37372b2de924ee605354b2aaefd
William <thenecroyeti@gmail.com>
2018-01-01 04:11:39 +0000

    RPC macro generates map function

--- lib/rai_ex/rpc.ex
+++ lib/rai_ex/rpc.ex
@@ -53,7 +53,7 @@ defmodule RaiEx.RPC do
       opts = Module.get_attribute(__MODULE__, :"#{@current_action}_opts") || []
 
       Module.eval_quoted __ENV__, [
-        RPC.__build_keyword_func__(@current_action, param_to_type_keywords, opts),
+        RPC.__build_map_func__(@current_action, param_to_type_keywords, opts),
         RPC.__build_seq_func__(@current_action, param_to_type_keywords, opts)
       ]
     end
@@ -74,13 +74,14 @@ defmodule RaiEx.RPC do
   end
 
   @doc false
-  def __build_keyword_func__(action, list, opts) do
-    quote do
-      def unquote(action) (unquote(RPC.__named_args_from_keyword__(__MODULE__, list))) do
-        RaiEx.Tools.Validator.validate_types!(unquote(list), unquote(RPC.__named_args_from_keyword__(__MODULE__, list)))
+  def __build_map_func__(action, list, opts) do
+    map = Macro.var(:map, __MODULE__)
 
-        unquote(RPC.__named_args_from_keyword__(__MODULE__, list))
-        |> Enum.into(%{})
+    quote do
+      def unquote(action) (unquote(map)) when is_map(unquote(map)) do
+        RaiEx.Tools.Validator.validate_types!(unquote(list), unquote(map))
+
+        unquote(map)
         |> Map.put(:action, unquote(action))
         |> Poison.encode!
         |> post_json_rpc(unquote(opts))
